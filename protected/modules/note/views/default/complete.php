4fa5ed1a3abd4e37abbd35e1cb20795f7c43792f
Zhandos Nuftiev <zhandos.90@gmail.com>
2013-08-03 04:28:13 +0500

    Правки на странице успешного создания ссылки

--- protected/modules/note/views/default/complete.php
+++ protected/modules/note/views/default/complete.php
@@ -1,5 +1,8 @@
-<div id="noteformw">
-	<div id="id_body_wrap" class="complete">
-		<?=Yii::t('app', 'Your link:')?> <strong><?=$this->createAbsoluteUrl('/note/default/view', array('path' => $path))?></strong>
-	</div>
-</div>
+<h3>
+	Copy this link and paste it into an email or instant message and send it to whom you want to read the note, or
+	<?=CHtml::link('destroy it now', $this->createAbsoluteUrl('/note/default/view', array('path' => $path)), array('id' => 'destroylink'))?>
+</h3>
+<input id="notelink" type="text" onclick="$(this).select()" readonly="readonly" size="50" value="<?=$this->createAbsoluteUrl('/note/default/view', array('path' => $path))?>">
+<h3 id="create">
+	<a href="/">Create another note</a>
+</h3>
